3f280af1e82c5d65e06acbe7c013403d5ff74c83
skarious <xxskariousxx@gmail.com>
2022-03-16 14:52:29 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1TIRSi5P0Uo_zlCl3BMTM2_5RXKBg8KMXjXtusNYcJTg/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyD9EOi0QemXvNZCiuKQKe4-q194ZQ2VPtA';
